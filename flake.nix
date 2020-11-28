bbaa77dc9945c76395773bb386d0af68bac3b2c6
Payas Relekar <relekarpayas@gmail.com>
2020-11-28 19:49:54 +0530

    Add Nix flake with devShell for reliabl environment setup next time

---
+++ flake.nix
@@ -0,0 +1,15 @@
+{
+  description = "Resume";
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages."${system}";
+    in {
+      devShell."${system}" = # architecture must be specified for devShell
+        pkgs.mkShell {
+          name = "resume";
+          buildInputs = with pkgs;[ texlive.combined.scheme-medium ];
+        };
+    };
+}
